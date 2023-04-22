2562a68fa6940a16591b4f79837a70e82f8fe984
Matt Howey <howey.matt@gmail.com>
2023-04-22 07:52:27 -0400

    Initial commit

---
+++ pages/devops-docker/index.tsx
@@ -0,0 +1,19 @@
+export default function DevOpsDockerPage() {
+  return (
+    <div>
+      <h2>DevOps with Docker Notes</h2>
+      <p>
+        The purpose of this site is to capture notes learned while taking this
+        DevOps course on Udemy:
+        <div className="linkWrap">
+          <a
+            href="https://www.udemy.com/course/devops-with-docker-kubernetes-and-azure-devops"
+            target="_blank"
+          >
+            Learn DevOps: Docker, Kubernetes, Terraform and Azure DevOps
+          </a>
+        </div>
+      </p>
+    </div>
+  );
+}
